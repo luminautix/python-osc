46b9b6cd4c67f318f4a4283b0795f43af5478d4f
luminautix <luminautix@users.noreply.github.com>
2017-06-19 16:42:35 +0200

    Create etc_eos_family_osc_example.py

---
+++ examples/etc_eos_family_osc_example.py
@@ -0,0 +1,19 @@
+""" OSC client for ETC EOS Family Lighting Control """
+
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ip", default="127.0.0.1", help="The ip of the OSC server")      
+  parser.add_argument("--port", type=int, default=5005, help="The port the OSC server is listening on")   
+  args = parser.parse_args()
+
+  client = udp_client.SimpleUDPClient(args.ip, args.port)
+  
+  client.send_message("eos/key/clear_cmdline", "" )
+  client.send_message("eos/key/go_to_cue", "" )
+  client.send_message("eos/key/8", "" )
+  client.send_message("eos/key/time", "" )
+  client.send_message("eos/key/0", "" )
+  client.send_message("eos/key/enter", "" )
